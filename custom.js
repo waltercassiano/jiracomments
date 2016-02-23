176e54d5cd16a3b715782b539724d8408c216dc0
acezario <acezario@ciandt.com>
2016-02-23 16:48:50 -0300

    Added files via upload
    1st change

--- custom.js
+++ custom.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var idForm = "#comment";
-  
+
+	
   $(idForm).submit(function( event ) {
     event.preventDefault();
     
@@ -16,11 +17,11 @@ $(document).ready(function() {
     html += 'h2. {color:#1B809E}Code Changes{color}' + doubleNewLine;
     
     // We have code changes?         
-    html += '*We have code changes?*' + newLine;
+    html += '*Do we have code changes?*' + newLine;
     html += $('input[name=code_changes]:checked', idForm).val() + doubleNewLine;
 
     // Changes Type:
-    html += '*Changes Type:*' + newLine;      
+    html += '*Technology:*' + newLine;      
     var code_changes_type = $('input:checkbox[name^=code_changes_type]:checked');
     if(code_changes_type.length > 0){
       code_changes_type.each(function(){
@@ -37,7 +38,7 @@ $(document).ready(function() {
     html += 'h2. {color:#CE4844}CMS Changes{color}' + doubleNewLine;
 
     // We have CMS changes?
-    html += '*We have CMS changes?*' + newLine;
+    html += '*Do we have CMS changes?*' + newLine;
     html += $('input[name=cms_changes]:checked', idForm).val() + doubleNewLine;
     
     // Steps
@@ -62,20 +63,98 @@ $(document).ready(function() {
     // ====== Unit Tests
     html += 'h2. {color:#AA6708}Unit Tests{color}' + doubleNewLine;
     var array_unit_tests_steps_title = [];
-    var array_unit_tests_steps_description = [];    
+    var array_unit_tests_steps_description = [];
+	var unit_tests_steps_steps = [];
+    
     $('input[name^="unit_tests_steps_title"]').each(function() {
       array_unit_tests_steps_title.push($(this).val());
     });    
     $('textarea[name^="unit_tests_steps_description"]').each(function() {
       array_unit_tests_steps_description.push($(this).val());
-    });    
+    });
+	$('textarea[name^="unit_tests_steps_steps"]').each(function() {
+      unit_tests_steps_steps.push($(this).val());
+    });      
     $('input[name^="unit_tests_steps_title"]').each(function(index) {
       html += '*' + (index + 1) + ') ' + array_unit_tests_steps_title[index] + '*' + newLine;
-      html += array_unit_tests_steps_description[index] + '*' + doubleNewLine;
+	  html += 'Steps :' + unit_tests_steps_steps[index] + '*' + doubleNewLine;
+      html += 'Expected Results :' + array_unit_tests_steps_description[index] + '*' + doubleNewLine;
     });  
     
     // ====== Result
     $("#result").val(html);
-    
+	    
   });
 });
+
+
+
+function showCodeChange(){
+    if(document.getElementById('codeChangeY').checked) {
+		$("#codeChange").show();
+	} else {
+		$("#codeChange").hide();
+	}
+}
+
+function showCMSChange(){
+    if(document.getElementById('cms_changes_yes').checked) {
+		$("#cmsSteps").show();
+	} else {
+		$("#cmsSteps").hide();
+	}
+}
+
+function validateForm(){
+	
+	var validation = true;
+	
+	if(!document.getElementById('cms_changes_yes').checked && !document.getElementById('cms_changes_no').checked) {
+		$("#result").hide();
+		$("#cmsError").show();
+		$('html, body').animate({ scrollTop: 0 }, 'fast');
+		validation =  false;
+		
+	}
+	else{		
+		$("#cmsError").hide();
+	}
+	
+	if(!document.getElementById('codeChangeY').checked && !document.getElementById('codeChangeN').checked) {
+		$("#codeError").show();
+		$("#result").hide();
+		$('html, body').animate({ scrollTop: 0 }, 'fast');
+		validation =  false;
+	}
+	else{		
+		$("#codeError").hide();
+	}
+	
+	return validation;
+
+}
+
+function showResult(){
+	
+	if(validateForm()){
+		
+		$("#result").show();
+		$("#cmsError").hide();
+		$("#cmsError").hide();
+	
+		var clipboard = new Clipboard('.btn');
+	}
+}
+
+var cloneCount =1;
+function addTest(){
+	
+	$("#unitTestForm").clone().appendTo( "#moreTests" ).attr('id', 'unitTestForm'+ cloneCount++).find("textarea").val("");
+}
+function remove(){
+
+    $(this).parent().remove();
+}
+
+
+
